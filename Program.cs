e65368925a430675180719eded91e4a2d7f5b22c
Zhanbolot <zhanbolotnr@gmail.com>
2022-03-22 23:46:03 +0600

    findmaximum

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace findMaximum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First number: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Second number: ");
+            int b = int.Parse(Console.ReadLine());
+            //1st 
+
+            /*int max = a;
+            if (b > a)
+                max = b;
+            */
+
+            //2nd
+
+            /* int max;
+             if (a > b)
+                 max = a;
+             else
+                 max = b;
+             */
+
+            //3-е решение
+            int max = a > b ? a : b; //тернарная операция
+
+            Console.WriteLine($"Maximum is: {max}");
+        }
+    }
+}
